e0f2d5eb1ddf1d8a7488e035e3767dff3966472b
Pedro Duran <pedrojosuedm@hotmail.com>
2023-12-29 13:49:12 -0400

    Initial Commit

---
+++ Scripts/Runtime/Core/MonoSingleton.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+namespace TinyModules.Core
+{
+    public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T _instance = null;
+
+        public static T Instance
+        {
+            get
+            {
+                if (!_instance)
+                    _instance = FindObjectOfType<T>();
+                
+                return _instance;
+            }
+        }
+
+        protected bool InitializeSingleton()
+        {
+            T current = this as T;
+            
+            if (_instance != null && _instance != current)
+            {
+                Destroy(current);
+                return false;
+            }
+
+            _instance = current;
+            return true;
+        }
+
+    }
+}
